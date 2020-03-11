17a3670954664a103cc0b534c1815334b80b6b9c
Wryn Wagner <wagner.wryn@gmail.com>
2020-03-11 13:56:47 -0600

    Updated test cases for duplicate word remover (python)

--- dictionary-replacer/python3/test.py
+++ dictionary-replacer/python3/test.py
@@ -4,11 +4,17 @@ from solution import Solution
 
 
 class MinimumRequiredTests(TestCase):
-    def test_dict_replacer_no_phrase(self):
+    def test_dict_replacer_empty_phrase(self):
         phrase = ''
         dictionary = {}
         result = Solution.dictionary_replacer(phrase, dictionary)
-        self.assertEqual('', result)
+        self.assertEqual(result, '')
+
+    def test_dict_replacer_no_replacements(self):
+        phrase = 'There are no replacements to be made here.'
+        dictionary = {}
+        result = Solution.dictionary_replacer(phrase, dictionary)
+        self.assertEqual(result, phrase)
 
     def test_dict_replacer_one_item_phrase(self):
         phrase = '<a>'
@@ -16,7 +22,7 @@ class MinimumRequiredTests(TestCase):
             'a': 'successful',
         }
         result = Solution.dictionary_replacer(phrase, dictionary)
-        self.assertEqual('successful', result)
+        self.assertEqual(result, 'successful')
 
     def test_dict_replacer_two_items_phrase(self):
         phrase = 'I think <1> lives in <2>.'
@@ -25,7 +31,16 @@ class MinimumRequiredTests(TestCase):
             '2': 'Idaho',
         }
         result = Solution.dictionary_replacer(phrase, dictionary)
-        self.assertEqual('I think Nate lives in Idaho.', result)
+        self.assertEqual(result, 'I think Nate lives in Idaho.')
+
+    def test_dict_replacer_two_identical_items_phrase(self):
+        phrase = 'I want to see my friend <friend_name>. <friend_name> lives in <state>.'
+        dictionary = {
+            'friend_name': 'Colton',
+            'state': 'Utah',
+        }
+        result = Solution.dictionary_replacer(phrase, dictionary)
+        self.assertEqual(result, 'I want to see my friend Colton. Colton lives in Utah.')
 
 
 class BonusTests(TestCase):
@@ -35,7 +50,7 @@ class BonusTests(TestCase):
             'b': 'non-null',
         }
         result = Solution.dictionary_replacer(phrase, dictionary)
-        self.assertEqual('<a>, non-null', result)
+        self.assertEqual(result, '<a>, non-null')
 
 
 if __name__ == '__main__':
