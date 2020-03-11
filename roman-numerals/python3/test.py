91edbcbed0e3a919b508aa0e871f5d7f9951f769
Wryn Wagner <wagner.wryn@gmail.com>
2020-03-11 14:26:29 -0600

    updated python tests for roman numeral converter

--- roman-numerals/python3/test.py
+++ roman-numerals/python3/test.py
@@ -6,61 +6,61 @@ from solution import Solution
 class MinimumRequiredTests(TestCase):
     def test_convert_integer_to_roman_numeral_one(self):
         roman_numeral = Solution.convert_integer_to_roman_numeral(1)
-        self.assertEqual('I', roman_numeral)
+        self.assertEqual(roman_numeral, 'I')
 
     def test_convert_integer_to_roman_numeral_three(self):
         roman_numeral = Solution.convert_integer_to_roman_numeral(3)
-        self.assertEqual('III', roman_numeral)
+        self.assertEqual(roman_numeral, 'III')
 
     def test_convert_integer_to_roman_numeral_four(self):
         roman_numeral = Solution.convert_integer_to_roman_numeral(4)
-        self.assertEqual('IV', roman_numeral)
+        self.assertEqual(roman_numeral, 'IV')
 
     def test_convert_integer_to_roman_numeral_five(self):
         roman_numeral = Solution.convert_integer_to_roman_numeral(5)
-        self.assertEqual('V', roman_numeral)
+        self.assertEqual(roman_numeral, 'V')
 
     def test_convert_integer_to_roman_numeral_six(self):
         roman_numeral = Solution.convert_integer_to_roman_numeral(6)
-        self.assertEqual('VI', roman_numeral)
+        self.assertEqual(roman_numeral, 'VI')
 
     def test_convert_integer_to_roman_numeral_nine(self):
         roman_numeral = Solution.convert_integer_to_roman_numeral(9)
-        self.assertEqual('IX', roman_numeral)
+        self.assertEqual(roman_numeral, 'IX')
 
     def test_convert_integer_to_roman_numeral_ten(self):
         roman_numeral = Solution.convert_integer_to_roman_numeral(10)
-        self.assertEqual('X', roman_numeral)
+        self.assertEqual(roman_numeral, 'X')
 
 
 class BonusTests(TestCase):
     def test_convert_roman_numeral_one(self):
-        roman_numeral = Solution.convert_roman_numeral_to_integer('I')
-        self.assertEqual(1, roman_numeral)
+        converted_integer = Solution.convert_roman_numeral_to_integer('I')
+        self.assertEqual(converted_integer, 1)
 
     def test_convert_roman_numeral_three(self):
-        roman_numeral = Solution.convert_roman_numeral_to_integer('III')
-        self.assertEqual(3, roman_numeral)
+        converted_integer = Solution.convert_roman_numeral_to_integer('III')
+        self.assertEqual(converted_integer, 3)
 
     def test_convert_roman_numeral_four(self):
-        roman_numeral = Solution.convert_roman_numeral_to_integer('IV')
-        self.assertEqual(4, roman_numeral)
+        converted_integer = Solution.convert_roman_numeral_to_integer('IV')
+        self.assertEqual(converted_integer, 4)
 
     def test_convert_roman_numeral_five(self):
-        roman_numeral = Solution.convert_roman_numeral_to_integer('V')
-        self.assertEqual(5, roman_numeral)
+        converted_integer = Solution.convert_roman_numeral_to_integer('V')
+        self.assertEqual(converted_integer, 5)
 
     def test_convert_roman_numeral_six(self):
-        roman_numeral = Solution.convert_roman_numeral_to_integer('VI')
-        self.assertEqual(6, roman_numeral)
+        converted_integer = Solution.convert_roman_numeral_to_integer('VI')
+        self.assertEqual(converted_integer, 6)
 
     def test_convert_roman_numeral_nine(self):
-        roman_numeral = Solution.convert_roman_numeral_to_integer('IX')
-        self.assertEqual(9, roman_numeral)
+        converted_integer = Solution.convert_roman_numeral_to_integer('IX')
+        self.assertEqual(converted_integer, 9)
 
     def test_convert_roman_numeral_ten(self):
-        roman_numeral = Solution.convert_roman_numeral_to_integer('X')
-        self.assertEqual(10, roman_numeral)
+        converted_integer = Solution.convert_roman_numeral_to_integer('X')
+        self.assertEqual(converted_integer, 10)
 
 
 if __name__ == '__main__':
