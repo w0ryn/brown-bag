a1d70ca9bb832b4b19c1263a971dee0c40b95d9c
Wryn Wagner <wagner.wryn@gmail.com>
2020-03-11 14:13:22 -0600

    Updated tests for duplicate word remover python

--- duplicate-words/python3/test.py
+++ duplicate-words/python3/test.py
@@ -7,17 +7,17 @@ class MinimumRequiredTests(TestCase):
     def test_remove_duplicate_words_none_exist(self):
         phrase = 'alpha'
         result = Solution.remove_duplicate_words(phrase)
-        self.assertEqual('alpha', result)
+        self.assertEqual(result, 'alpha')
 
     def test_remove_duplicate_words_one_duplicate(self):
         phrase = 'alpha beta alpha'
         result = Solution.remove_duplicate_words(phrase)
-        self.assertEqual('alpha beta', result)
+        self.assertEqual(result, 'alpha beta')
 
     def test_remove_duplicate_words_multiple_duplicates(self):
         phrase = 'alpha beta alpha alpha beta delta alpha'
         result = Solution.remove_duplicate_words(phrase)
-        self.assertEqual('alpha beta delta', result)
+        self.assertEqual(result, 'alpha beta delta')
 
 if __name__ == '__main__':
     Test.run_suite(
