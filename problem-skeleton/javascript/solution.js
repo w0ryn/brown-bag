e6222f4d02317d81916259911b7a1b954978e06d
Wryn Wagner <wagner.wryn@gmail.com>
2020-03-11 12:20:34 -0600

    Added problem skeleton for javascript

---
+++ problem-skeleton/javascript/solution.js
@@ -0,0 +1,2 @@
+const Solution = {}
+exports.solution = Solution;
---
+++ problem-skeleton/javascript/test.js
@@ -0,0 +1,17 @@
+const Utils = require('./local_utils.js').utils;
+const Solution = require('./solution.js').solution;
+
+// assertion facility
+const assertEqual  = Utils.test.assertEqual;
+const assertTrue   = Utils.test.assertTrue;
+const assertFalse  = Utils.test.assertFalse;
+const assertTruthy = Utils.test.assertTruthy;
+const assertFalsey = Utils.test.assertFalsey;
+
+
+Utils.test.runSuite(
+  'MinimumRequiredTests', [
+  ],
+  'BonusTests', [
+  ],
+);
